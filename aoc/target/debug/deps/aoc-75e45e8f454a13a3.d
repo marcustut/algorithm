41f0f3b64f99f71e448c83d608fd243939480df4
Marcus Lee <marcustutorial@hotmail.com>
2021-12-13 01:01:47 +0800

    deleted target folder

--- aoc/target/debug/deps/aoc-75e45e8f454a13a3.d
+++
@@ -1,9 +0,0 @@
-/Users/marcus/Projects/algorithm/aoc/target/debug/deps/aoc-75e45e8f454a13a3: src/main.rs src/utils/mod.rs src/days/mod.rs src/days/day1.rs src/days/day2.rs
-
-/Users/marcus/Projects/algorithm/aoc/target/debug/deps/aoc-75e45e8f454a13a3.d: src/main.rs src/utils/mod.rs src/days/mod.rs src/days/day1.rs src/days/day2.rs
-
-src/main.rs:
-src/utils/mod.rs:
-src/days/mod.rs:
-src/days/day1.rs:
-src/days/day2.rs:
