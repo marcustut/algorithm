f46493e800b1466302b2e1065643200de767ae92
Marcus Lee <marcustutorial@hotmail.com>
2023-12-02 12:00:33 +0000

    feat: refactored aoc day 2

--- aoc2023/day2.cpp
+++ aoc2023/day2.cpp
@@ -9,6 +9,9 @@ using namespace std;
 #define FMTPRINT(...) ;
 #endif
 
+#define RED_REGEX "(\\d+) red"
+#define GREEN_REGEX "(\\d+) green"
+#define BLUE_REGEX "(\\d+) blue"
 #define GAME_REGEX "Game (\\d+): (.*)"
 #define MAX_RED 12
 #define MAX_GREEN 13
@@ -31,62 +34,55 @@ vector<string> split(string s, string delimiter) {
 
 void solve1(const string& inpath) {
   auto lines = read_file_lines(inpath);
-  long long sum = 0;
-  for_each(
-      lines.begin(),
-      lines.end(),
-      [&](string line) {
-        long long id = 0;
-        regex game_regex(GAME_REGEX);
-        smatch matches;
+  FMTPRINT("{}\n", transform_reduce(lines.begin(), lines.end(), 0, plus{}, [&](string line) {
+             int id = 0;
+             regex game_regex(GAME_REGEX);
+             smatch matches;
 
-        if (regex_search(line, matches, game_regex))
-          id = stoi(matches[1].str());
+             if (regex_search(line, matches, game_regex))
+               id = stoi(matches[1].str());
 
-        auto tokens = split(matches[2].str(), ";");
-        for (auto& token : tokens) {
-          smatch m;
-          long long red(0), green(0), blue(0);
-          if (regex_search(token, m, regex("(\\d+) red"))) red = stoi(m[1].str());
-          if (regex_search(token, m, regex("(\\d+) green"))) green = stoi(m[1].str());
-          if (regex_search(token, m, regex("(\\d+) blue"))) blue = stoi(m[1].str());
-          if (red > MAX_RED || green > MAX_GREEN || blue > MAX_BLUE) return;
-        }
-        sum += id;
-      });
-  FMTPRINT("{}\n", sum);
+             auto tokens = split(matches[2].str(), ";");
+             for (auto& token : tokens) {
+               smatch m;
+               long long red(0), green(0), blue(0);
+               if (regex_search(token, m, regex(RED_REGEX))) red = stoi(m[1].str());
+               if (regex_search(token, m, regex(GREEN_REGEX))) green = stoi(m[1].str());
+               if (regex_search(token, m, regex(BLUE_REGEX))) blue = stoi(m[1].str());
+               if (red > MAX_RED || green > MAX_GREEN || blue > MAX_BLUE) return 0;
+             }
+
+             return id;
+           }));
 }
 
 void solve2(const string& inpath) {
   auto lines = read_file_lines(inpath);
-  long long sum = 0;
-  for_each(
-      lines.begin(),
-      lines.end(),
-      [&](string line) {
-        regex game_regex(GAME_REGEX);
-        smatch matches;
-        regex_search(line, matches, game_regex);
+  FMTPRINT("{}\n", transform_reduce(lines.begin(), lines.end(), 0, plus{}, [&](string line) {
+             regex game_regex(GAME_REGEX);
+             smatch matches;
+             regex_search(line, matches, game_regex);
 
-        auto tokens = split(matches[2].str(), ";");
-        int red(0), green(0), blue(0);
-        for (auto& token : tokens) {
-          smatch m;
-          if (regex_search(token, m, regex("(\\d+) red"))) red = max(red, stoi(m[1].str()));
-          if (regex_search(token, m, regex("(\\d+) green"))) green = max(green, stoi(m[1].str()));
-          if (regex_search(token, m, regex("(\\d+) blue"))) blue = max(blue, stoi(m[1].str()));
-        }
-        sum += red * green * blue;
-      });
-  FMTPRINT("{}\n", sum);
+             auto tokens = split(matches[2].str(), ";");
+             int red(0), green(0), blue(0);
+             for (auto& token : tokens) {
+               smatch m;
+               if (regex_search(token, m, regex(RED_REGEX))) red = max(red, stoi(m[1].str()));
+               if (regex_search(token, m, regex(GREEN_REGEX))) green = max(green, stoi(m[1].str()));
+               if (regex_search(token, m, regex(BLUE_REGEX))) blue = max(blue, stoi(m[1].str()));
+             }
+             return red * green * blue;
+           }));
 }
 
 int main() {
-  // vector<string> files1 = {"tests/day2_1_sample.txt", "tests/day2_1.txt"};
+  FMTPRINT("Part 1\n");
+  vector<string> files1 = {"tests/day2_1_sample.txt", "tests/day2_1.txt"};
 
-  // for (auto& file : files1)
-  //   solve1(file);
+  for (auto& file : files1)
+    solve1(file);
 
+  FMTPRINT("Part 2\n");
   vector<string> files2 = {"tests/day2_1_sample.txt", "tests/day2_1.txt"};
 
   for (auto& file : files2)
